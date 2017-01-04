e35fe0c775a5c69d497ab1e45ba0f739c788afcb
jiangjun <jiangjun@baidu.com>
2017-01-04 19:23:17 +0800

    initial

--- src/algorithm/regression/LinearRegress.h
+++ src/algorithm/regression/LinearRegress.h
@@ -18,10 +18,23 @@ class LinearRegression{
 public:
     /*
      * simple linear regression, samples >> features is need.
+     * sum(yi - xiTw)^2 [i = i-->m]
+     * w^ = (xTx)-1 * xTy
      */
     template<class T>
     bool standard_regression(ccma::algebra::LabeledDenseMatrixT<T>* train_data, ccma::algebra::DenseColMatrixT<real>* weights);
 
+    /*
+     * locally weighted linear regression.
+     * to set a personal weight for every train data nearby predict data point
+     * w^ = (xTWX)-1 * xTWy
+     * gaussian kernal: w(i, i) = exp(|x(i)-x| / 2k^2)
+     */
+    template<class T>
+    bool local_weight_logistic_regresion(ccma::algebra::LabeledDenseMatrixT<T>* train_data,
+                                         ccma::algebra::DenseMatrixT<T>* predict_data,
+                                         real k,
+                                         ccma::algebra::DenseColMatrixT<real>* predict_labels);
 
 };//class LinearRegression
 
