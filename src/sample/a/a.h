cb275d4183c45bbbc8e4d2c4f1da3fcd84d108a4
jiangjun <jiangjun@baidu.com>
2017-01-06 19:21:55 +0800

    delete temp file

--- src/sample/a/a.h
+++
@@ -1,9 +0,0 @@
-
-class A{
-public:
-    template<class T>
-    T sum(T a, T b);
-
-    template<class T>
-    A* sumsum(T a, T b);
-};
--- src/sample/b/b.h
+++
@@ -1,6 +0,0 @@
-#include "a/a.h"
-
-class B{
-public:
-    int double_sum(int a, int b);
-};
