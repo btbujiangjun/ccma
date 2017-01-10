cccc48bbbf0d32dffe7abb95e3ba61835585ce4c
jiangjun <jiangjun@baidu.com>
2017-01-10 15:50:38 +0800

    initail

--- src/algorithm/tree/CART.h
+++ src/algorithm/tree/CART.h
@@ -16,6 +16,7 @@ namespace algorithm{
 namespace tree{
 
 class CartTree{
+private:
 };//class CartTree
 
 class ClassificationAndRegressionTree{
--- src/algorithm/tree/DecisionTree.h
+++ src/algorithm/tree/DecisionTree.h
@@ -18,8 +18,21 @@ namespace tree{
 
 class DecisionTreeModel{
 public:
-    DecisionTreeModel(const std::string& name, const std::string& value);
-    ~DecisionTreeModel();
+    DecisionTreeModel(const std::string& name, const std::string& value):_name(name), _value(value){
+        _children = new std::vector<DecisionTreeModel*>();
+        _is_leaf = true;
+        _is_root = true;
+    }
+    ~DecisionTreeModel(){
+        typename std::vector<DecisionTreeModel*>::iterator it = _children->begin();
+        while(it != _children->end()){
+            DecisionTreeModel* node = *it;
+            delete node;
+            it = _children->erase(it);
+        }
+        _children->clear();
+        _children = nullptr;
+    }
 
     inline std::string get_name() const{
         return _name;
@@ -29,7 +42,11 @@ public:
         return _value;
     }
 
-    void add_child(DecisionTreeModel* child);
+    void add_child(DecisionTreeModel* child){
+        child->set_root(false);
+        _children->push_back(child);
+        _is_leaf = false;
+    }
 
     inline bool is_leaf() const{
         return _is_leaf;
@@ -57,7 +74,21 @@ private:
     bool _is_root;
     std::vector<DecisionTreeModel*>* _children;
 
-    void display(const std::string prefix, DecisionTreeModel* node);
+    void display(const std::string prefix, DecisionTreeModel* node){
+        if(node->is_root()){
+            printf("%sroot:[%s]samples:[%s]\n", prefix.c_str(), node->get_name().c_str(), node->get_value().c_str());
+        }else if(node->is_leaf()){
+            printf("%slabel:[%s]samples:[%s]\n", prefix.c_str(), node->get_name().c_str(), node->get_value().c_str());
+        }else{
+            printf("%sfeature:[%s]value:[%s]\n", prefix.c_str(), node->get_name().c_str(), node->get_value().c_str());
+        }
+
+        std::vector<DecisionTreeModel*>* children = node->get_children();
+        typename std::vector<DecisionTreeModel*>::iterator it = children->begin();
+        while(it != children->end()){
+            display(prefix+"\t", *it++);
+        }
+    }
 };
 
 class DecisionTree{
