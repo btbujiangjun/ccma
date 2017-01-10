7b23cad094bf4fa0b9e95e9d93da3522acc1d632
jiangjun <jiangjun@baidu.com>
2017-01-10 15:50:42 +0800

    initail

--- src/algorithm/tree/DecisionTree.cpp
+++ src/algorithm/tree/DecisionTree.cpp
@@ -12,46 +12,6 @@ namespace ccma{
 namespace algorithm{
 namespace tree{
 
-DecisionTreeModel::DecisionTreeModel(const std::string& name, const std::string& value) : _name(name), _value(value){
-    _children = new std::vector<DecisionTreeModel*>();
-    _is_leaf = true;
-    _is_root = true;
-}
-
-DecisionTreeModel::~DecisionTreeModel(){
-    typename std::vector<DecisionTreeModel*>::iterator it = _children->begin();
-    while(it != _children->end()){
-        DecisionTreeModel* node = *it;
-        delete node;
-        it = _children->erase(it);
-    }
-    _children->clear();
-    _children = nullptr;
-}
-
-void DecisionTreeModel::add_child(DecisionTreeModel* child){
-    child->set_root(false);
-    _children->push_back(child);
-    _is_leaf = false;
-}
-
-void DecisionTreeModel::display(const std::string prefix, DecisionTreeModel* node){
-    if(node->is_root()){
-        printf("%sroot:[%s]samples:[%s]\n", prefix.c_str(), node->get_name().c_str(), node->get_value().c_str());
-    }else if(node->is_leaf()){
-        printf("%slabel:[%s]samples:[%s]\n", prefix.c_str(), node->get_name().c_str(), node->get_value().c_str());
-    }else{
-        printf("%sfeature:[%s]value:[%s]\n", prefix.c_str(), node->get_name().c_str(), node->get_value().c_str());
-    }
-
-    std::vector<DecisionTreeModel*>* children = node->get_children();
-    typename std::vector<DecisionTreeModel*>::iterator it = children->begin();
-    while(it != children->end()){
-        display(prefix+"\t", *it++);
-    }
-}
-
-
 template<class T>
 void DecisionTree::train(ccma::algebra::LabeledDenseMatrixT<T>* train_data, DecisionTreeModel* model){
     create_tree(train_data, model);
