9ae700c2323e1f748dcab84e3bddcbae421c984b
jiangjun <jiangjun@baidu.com>
2017-04-19 19:15:21 +0800

    delete

--- src/utils/MnistHelper.h
+++ src/utils/MnistHelper.h
@@ -10,21 +10,79 @@
 #define _CCMA_UTILS_MNISTHELPER_H_
 
 #include <fstream>
+#include <iostream>
+#include <memory>
 #include <string>
 #include "algebra/BaseMatrix.h"
 
 namespace ccma{
-namespace util{
+namespace utils{
 class MnistHelper{
 public:
-    void read_mnist_file(const std::string& path, uint32_t key);
+    template<class T>
+    ccma::algebra::LabeledDenseMatrixT<T>* read(const std::string& image_file,
+                                                           const std::string& label_file,
+                                                           const int limit = -1);
 
 private:
+    inline std::unique_ptr<char[]> read_mnist_file(const std::string& path,
+                                                   uint32_t key,
+                                                   uint32_t* out_rows);
     inline uint32_t read_header(const std::unique_ptr<char[]>& buffer, size_t position);
 };//class MnistHelper
 
+template<class T>
+ccma::algebra::LabeledDenseMatrixT<T>* MnistHelper::read(const std::string& image_file,
+            const std::string& label_file,
+            const int limit){
 
-void MnistHelper::read_mnist_file(const std::string& path, uint32_t key){
+    ccma::algebra::LabeledDenseMatrixT<T>* mat;
+
+    uint32_t image_num = 0;
+    uint32_t label_num = 0;
+    auto image_buffer = read_mnist_file(image_file, 0x803, &image_num);
+    auto label_buffer = read_mnist_file(label_file, 0x801, &label_num);
+    if(!image_buffer || !label_buffer || image_num != label_num){
+        mat = new ccma::algebra::LabeledDenseMatrixT<T>();
+        return mat;
+    }
+
+    auto count = image_num;
+    if( limit > 0 && limit < count){
+        count = limit;
+    }
+
+    //read image data
+    auto rows = read_header(image_buffer, 2);
+    auto cols = read_header(image_buffer, 3);
+
+    auto image_data_buffer = reinterpret_cast<unsigned char*>(image_buffer.get() + 16);
+
+    T* data = new T[count * rows * cols];
+    for(size_t i = 0; i < count * rows * cols; i++){
+        auto pixel = *image_data_buffer++;
+        data[i] = static_cast<T>(pixel);
+    }
+
+    //read label data
+    auto label_data_buffer = reinterpret_cast<unsigned char*>(label_buffer.get() + 8);
+
+    T* labels = new T[count];
+    for(size_t i = 0; i < count; i++){
+        auto label = *label_data_buffer++;
+        labels[i] = static_cast<T>(label);
+    }
+
+    mat = new ccma::algebra::LabeledDenseMatrixT<T>(data, labels, count, rows * cols);
+    return mat;
+}
+
+inline std::unique_ptr<char[]> MnistHelper::read_mnist_file(const std::string& path,
+    uint32_t key,
+    uint32_t* out_rows){
+
+    *out_rows = 0;
+
     std::ifstream file;
     file.open(path, std::ios::in | std::ios::binary | std::ios::ate);
 
@@ -49,6 +107,25 @@ void MnistHelper::read_mnist_file(const std::string& path, uint32_t key){
     }
 
     auto count = read_header(buffer, 1);
+
+    if(magic_num == 0x803){
+        auto rows = read_header(buffer, 2);
+        auto cols = read_header(buffer, 3);
+
+        if(size < count * rows * cols + 16){
+            std::cout << "File size ERROR" << std::endl;
+            return {};
+        }
+    }else if( magic_num == 0x801){
+        if(size < count + 8){
+            std::cout << "File size ERROR" << std::endl;
+            return {};
+        }
+    }
+
+    *out_rows = count;
+
+    return std::move(buffer);
 }
 
 inline uint32_t MnistHelper::read_header(const std::unique_ptr<char[]>& buffer, size_t position){
---
+++ src/utils/TestMnistHelper.cpp
@@ -0,0 +1,22 @@
+/*********************************************
+* Author: Jun Jiang - jiangjun4@sina.com
+* Created: 2017-04-19 15:54
+* Last modified: 2017-04-19 15:54
+* Filename: TestMnistHelper.cpp
+* Description: 
+**********************************************/
+#include "MnistHelper.h"
+
+int main(int argc, char** argv){
+    ccma::utils::MnistHelper helper;
+    ccma::algebra::LabeledDenseMatrixT<int>* mat;
+
+    mat = helper.read<int>("data/mnist/train-images-idx3-ubyte", "data/mnist/train-labels-idx1-ubyte", 1);
+    mat->display();
+    delete mat;
+
+//    mat = helper.read_mnist_data<int>("data/mnist/train-images-idx3-ubyte", "data/mnist/train-labels-idx1-ubyte", 10);
+//    mat->display();
+
+//    delete mat;
+}
