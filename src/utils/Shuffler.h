c331f405034dfbd8ef81979685f62c8370bd5409
jiangjun <jiangjun@baidu.com>
2017-04-20 18:23:28 +0800

    update

---
+++ src/utils/Shuffler.h
@@ -0,0 +1,79 @@
+/*********************************************
+* Author: Jun Jiang - jiangjun4@sina.com
+* Created: 2017-04-10 16:33
+* Last modified: 2017-04-10 16:33
+* Filename: MatrixShuffle.h
+* Description: matrix shuffle
+**********************************************/
+
+#ifndef _CCMA_UTILS_SHUFFLER_H_
+#define _CCMA_UTILS_SHUFFLER_H_
+
+#include <random>
+#include <vector>
+#include <ctime>
+
+namespace ccma{
+namespace utils{
+
+class Shuffler{
+public:
+    Shuffler(uint size);
+
+    ~Shuffler();
+
+    void shuffle();
+
+    uint get_row(uint row_id);
+
+private:
+    uint _size;
+    std::vector<uint> _shuffer_idx;
+};//class Shuffler
+
+
+Shuffler::Shuffler(uint size){
+    _size = size;
+    shuffle();
+}
+
+Shuffler::~Shuffler(){
+    _shuffle_idx.clear();
+}
+
+void Shuffler::shuffle(){
+
+    _shuffle_idx.clear();
+
+    std::vector<int> idx;
+    for(int i = 0; i < _size; i++){
+        idx.push_back(i);
+    }
+
+    while(idx.size() > 0){
+        std::default_random_engine generator(time(0));
+        std::uniform_int_distribution<int> dis(0, idx.size() - 1);
+
+        int idx_value = idx[dis(generator)];
+        _shuffle_idx.push_back(idx[idx_value]);
+
+        std::vector<int>::iterator it = idx.begin();
+        while(it != idx.end()){
+            if(*it == idx_value){
+                idx.erase(it);
+                break;
+            }
+            it++;
+        }
+    }
+
+}
+
+uint Shuffler<T>::get_row(int row_id){
+    return _shuffle_idx[row_id];
+}
+
+}//namespace algebra
+}//namespace ccma
+
+#endif
