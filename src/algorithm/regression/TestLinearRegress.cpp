c8da073218e7c348a06279c9994b2a1cce8d1461
baidu <baidu@MacBook-Pro.lan>
2017-05-07 01:51:01 +0800

    fix read file

--- src/algorithm/regression/TestLinearRegress.cpp
+++ src/algorithm/regression/TestLinearRegress.cpp
@@ -13,7 +13,7 @@
 int main(int argc, char** argv){
     ccma::algebra::LabeledDenseMatrixT<real>* lmat = new ccma::algebra::LabeledDenseMatrixT<real>();
     ccma::utils::DenseFileOp* fo = new ccma::utils::DenseFileOp();
-    if(fo->read_data("./data/ex0.txt", lmat)){
+    if(fo->read_data<real>("./data/ex0.txt", lmat)){
         ccma::algorithm::regression::LinearRegression* regression = new ccma::algorithm::regression::LinearRegression();
         ccma::algebra::DenseColMatrixT<real>* weights = new ccma::algebra::DenseColMatrixT<real>(lmat->get_cols(), 0.0);
         if(regression->standard_regression<real>(lmat, weights)){
