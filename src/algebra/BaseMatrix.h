5c72bfc6ab258eac3cc56cc9e053fa5e5fad49be
baidu <baidu@MacBook-Pro.lan>
2017-05-07 23:18:07 +0800

    adjust num_per_thread

--- src/algebra/BaseMatrix.h
+++ src/algebra/BaseMatrix.h
@@ -125,7 +125,7 @@ protected:
     uint _rows;
     uint _cols;
 
-    uint _num_per_thread = 500;
+    uint _num_per_thread = 500000;
 private:
     const uint _num_hardware_concurrency = std::thread::hardware_concurrency();
 };//class BaseMatrixT
