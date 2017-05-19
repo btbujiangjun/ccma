2c744cc4b8fd78a354606bd3465c21c68ddcf761
jiangjun <jiangjun@baidu.com>
2017-05-19 15:48:01 +0800

    update code style

--- src/algorithm/tree/TestCART.cpp
+++ src/algorithm/tree/TestCART.cpp
@@ -12,12 +12,12 @@
 
 int main(int argc, char** argv){
     uint* d = new uint[0];
-    ccma::utils::DenseFileOp* file_op = new ccma::utils::DenseFileOp();
-    ccma::algebra::LabeledDenseMatrixT<real>* mat = new ccma::algebra::LabeledDenseMatrixT<real>();
+    auto file_op = new ccma::utils::DenseFileOp();
+    auto mat = new ccma::algebra::LabeledDenseMatrixT<real>();
 
     if(file_op->read_data("./data/ex0.txt", mat)){
-        ccma::algorithm::tree::ClassificationAndRegressionTree* reg_tree = new ccma::algorithm::tree::ClassificationAndRegressionTree();
-        ccma::algorithm::tree::CartModel* model = new ccma::algorithm::tree::CartModel();
+        auto reg_tree = new ccma::algorithm::tree::ClassificationAndRegressionTree();
+        auto model = new ccma::algorithm::tree::CartModel();
 
         reg_tree->train(mat, 1, 0, model);
         model->display();
