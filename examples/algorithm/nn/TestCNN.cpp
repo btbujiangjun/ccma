387f7b779ac99779372e1aaa5a80fb8f33448173
jiangjun <jiangjun@baidu.com>
2017-06-02 18:52:11 +0800

    initialize

--- examples/algorithm/nn/TestCNN.cpp
+++ examples/algorithm/nn/TestCNN.cpp
@@ -17,17 +17,20 @@ int main(int argc, char** argv){
     cnn->add_layer(new ccma::algorithm::cnn::FullConnectionLayer(10));
 
     ccma::utils::MnistHelper<real> helper;
+    const uint training_cnt = 1;
+    const uint test_cnt = 1;
     auto train_data     = new ccma::algebra::DenseMatrixT<real>();
-    helper.read_image("data/mnist/train-images-idx3-ubyte",train_data, -1);
+    helper.read_image("data/mnist/train-images-idx3-ubyte",train_data, training_cnt);
     auto train_label    = new ccma::algebra::DenseMatrixT<real>();
-    helper.read_vec_label("data/mnist/train-labels-idx1-ubyte", train_label, -1);
+    helper.read_vec_label("data/mnist/train-labels-idx1-ubyte", train_label, training_cnt);
 
     auto test_data      = new ccma::algebra::DenseMatrixT<real>();
-    helper.read_image("data/mnist/t10k-images-idx3-ubyte", test_data, -1);
+    helper.read_image("data/mnist/t10k-images-idx3-ubyte", test_data, test_cnt);
 
     auto test_label     = new ccma::algebra::DenseMatrixT<real>();
-    helper.read_label("data/mnist/t10k-labels-idx1-ubyte", test_label, -1);
-    cnn->train(train_data, train_label, 2, test_data, test_label);
+    helper.read_label("data/mnist/t10k-labels-idx1-ubyte", test_label, test_cnt);
+    //cnn->train(train_data, train_label, 10, test_data, test_label);
+    cnn->train(train_data, train_label, 5);
 
     delete cnn;
     delete train_data;
