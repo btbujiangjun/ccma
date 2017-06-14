65ae0d7264a14a69a2a0ebdf4c1718ac078d147d
btbujiangjun <jiangjun4@sina.com>
2017-06-14 18:44:18 +0800

    initialize

--- src/algorithm/regression/LinearRegress.cpp
+++ src/algorithm/regression/LinearRegress.cpp
@@ -17,46 +17,41 @@ namespace regression{
 template<class T>
 bool LinearRegression::standard_regression(ccma::algebra::LabeledDenseMatrixT<T>* train_data, ccma::algebra::DenseColMatrixT<real>* weights){
 
+    bool result_value = false;
+
     auto x = new ccma::algebra::DenseMatrixT<T>();
     train_data->clone(x);
 
-    auto y = new ccma::algebra::DenseMatrixT<T>();
-    train_data->get_labels(y);
+    auto xT = new ccma::algebra::DenseMatrixT<T>();
+    x->clone(xT);
+    xT->transpose();
 
-    ccma::algebra::BaseMatrixT<T>* xT = new ccma::algebra::DenseMatrixT<T>();
-    _helper->transpose(x, xT);
+    //xTx = x .* x.T
+    auto xTx = new ccma::algebra::DenseMatrixT<T>();
+    xT->clone(xTx);
+    xTx->dot(x);
 
-    ccma::algebra::BaseMatrixT<T>* xTx = new ccma::algebra::DenseMatrixT<T>();
-    if(_helper->dot(xT, x, xTx)){
-        delete x;
-	delete y;
-	delete xT;
-	delete xTx;
-        return false;
+    auto xTxI = new ccma::algebra::DenseMatrixT<real>();
+    //xTx.inverse()
+    if(xTx->inverse(xTxI)){
+        auto y = new ccma::algebra::DenseMatrixT<T>();
+        train_data->get_labels(y);
+
+        xT->dot(y);//xTy
+        xTxI->dot(xT);//xTxI .* xTy
+        xTxI->clone(weights);
+        
+        delete y;
+
+        result_value = true;
     }
-
-    ccma::algebra::BaseMatrixT<real>* xTxI = new ccma::algebra::DenseMatrixT<real>();
-    if(!xTx->inverse(xTxI)){
-        delete x;
-	delete y;
-	delete xT;
-	delete xTx;
-	delete xTxI;
-        return false;
-    }
-
-    ccma::algebra::BaseMatrixT<T>* xTy = new ccma::algebra::DenseMatrixT<T>();
-    _helper->dot(xT, y, xTy);
-    _helper->dot(xTxI, xTy, weights);
 
     delete x;
-    delete y;
     delete xT;
     delete xTx;
-    delete xTy;
     delete xTxI;
 
-    return true;
+    return result_value;
 }
 
 
@@ -77,65 +72,69 @@ bool LinearRegression::local_weight_logistic_regression(ccma::algebra::LabeledDe
     auto y = new ccma::algebra::DenseMatrixT<T>();
     train_data->get_labels(y);
 
-    ccma::algebra::DenseMatrixT<T>* xT = new ccma::algebra::DenseMatrixT<T>();
-    _helper->transpose(x, xT);
+    auto xT = new ccma::algebra::DenseMatrixT<T>();
+    x->clone(xT);
+    xT->transpose();
 
-    for(uint i = 0 ; i < predict_data->get_rows(); i++){
-        ccma::algebra::BaseMatrixT<real>* weight = new ccma::algebra::DenseEyeMatrixT<real>(train_data->get_rows());
+    uint predict_row = predict_data->get_rows();
+    uint train_row = train_data->get_rows();
+
+    for(uint i = 0 ; i != predict_row; i++){
+        auto weight = new ccma::algebra::DenseEyeMatrixT<real>(train_data->get_rows());
         auto predict_row_mat = new ccma::algebra::DenseMatrixT<T>();
         predict_data->get_row_data(i, predict_row_mat);
 
-        for(uint j = 0; j < train_data->get_rows(); j++){
+        for(uint j = 0; j != train_row; j++){
             auto train_row_mat = new ccma::algebra::LabeledDenseMatrixT<T>();
             train_data->get_row_data(j, train_row_mat);
 
-            ccma::algebra::DenseMatrixT<T>* diff_mat = new ccma::algebra::DenseMatrixT<T>();
+            auto diff_mat = new ccma::algebra::DenseMatrixT<T>();
             _helper->subtract(predict_row_mat, train_row_mat, diff_mat);
 
-            ccma::algebra::DenseMatrixT<T>* diff_mat_t = new ccma::algebra::DenseMatrixT<T>();
+            auto diff_mat_t = new ccma::algebra::DenseMatrixT<T>();
             _helper->transpose(diff_mat, diff_mat_t);
 
-            ccma::algebra::DenseMatrixT<T>* diff_mat_diff_mat_t = new ccma::algebra::DenseMatrixT<T>();
+            auto diff_mat_diff_mat_t = new ccma::algebra::DenseMatrixT<T>();
             _helper->dot(diff_mat, diff_mat_t, diff_mat_diff_mat_t);
 
             //personalized weight for every train data with gaussian kernal
             weight->set_data(exp((real)diff_mat_diff_mat_t->get_data(0, 0) / (-2.0 * k * k)), j, j);
             delete train_row_mat;
-	    delete diff_mat;
-	    delete diff_mat_t;
-	    delete diff_mat_diff_mat_t;
+	        delete diff_mat;
+    	    delete diff_mat_t;
+	        delete diff_mat_diff_mat_t;
         }
 
-        ccma::algebra::DenseMatrixT<real>* weight_x = new ccma::algebra::DenseMatrixT<real>();
+        auto weight_x = new ccma::algebra::DenseMatrixT<real>();
         _helper->dot(weight, x, weight_x);
 
-        ccma::algebra::DenseMatrixT<real>* xTx = new ccma::algebra::DenseMatrixT<real>();
+        auto xTx = new ccma::algebra::DenseMatrixT<real>();
         _helper->dot(xT, weight_x, xTx);
 
         real det = 0.0;
         if(!xTx->det(&det) || det == 0.0){
             delete x;
-	    delete y;
-	    delete xT;
-	    delete weight_x;
-	    delete xTx;
-	    delete predict_row_mat;
+	        delete y;
+    	    delete xT;
+	        delete weight_x;
+	        delete xTx;
+    	    delete predict_row_mat;
             return false;
         }
 
-        ccma::algebra::DenseMatrixT<real>* weight_y = new ccma::algebra::DenseMatrixT<real>();
+        auto weight_y = new ccma::algebra::DenseMatrixT<real>();
         _helper->dot(weight, y, weight_y);
 
-        ccma::algebra::DenseMatrixT<real>* xT_weight_y = new ccma::algebra::DenseMatrixT<real>();
+        auto xT_weight_y = new ccma::algebra::DenseMatrixT<real>();
         _helper->dot(xT, weight_y, xT_weight_y);
 
-        ccma::algebra::DenseMatrixT<real>* xTxI = new ccma::algebra::DenseMatrixT<real>();
+        auto xTxI = new ccma::algebra::DenseMatrixT<real>();
         xTx->inverse(xTxI);
 
-        ccma::algebra::DenseMatrixT<real>* weight_i = new ccma::algebra::DenseMatrixT<real>();
+        auto weight_i = new ccma::algebra::DenseMatrixT<real>();
         _helper->dot(xTxI, xT_weight_y, weight_i);
 
-        ccma::algebra::DenseMatrixT<real>* predict_mat_i = new ccma::algebra::DenseMatrixT<real>();
+        auto predict_mat_i = new ccma::algebra::DenseMatrixT<real>();
         _helper->dot(predict_row_mat, weight_i, predict_mat_i);
 
         labels[i] = predict_mat_i->get_data(0);
