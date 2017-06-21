909fe52f151f4e500c04499bbada26935723279a
btbujiangjun <jiangjun4@sina.com>
2017-06-21 18:45:43 +0800

    initialize

--- src/algorithm/rnn/Layer.cpp
+++ src/algorithm/rnn/Layer.cpp
@@ -12,15 +12,60 @@ namespace ccma{
 namespace algorithm{
 namespace rnn{
 
-bool DataLayer::initialize(){
-	return true;
+void Layer::initialize(){
+	for(auto&& mat : _store){
+		delete mat;
+	}
+	_store.clear();
+
+	for(auto&& mat : _activation){
+		delete mat;
+	}
+	_activation.clear();
 }
 
-void DataLayer::feed_farword(Layer* pre_layer, bool debug){
-	if(_store == nullptr){
-		_store = new ccma::algebra::BaseMatrix<real>();
+void feed_farward(ccma::algebra::BaseMatrixT<real>* train_seq_data, bool debug){
+	
+	initialize();
+
+	uint seq_size = train_seq_data->get_rows();
+
+	auto seq_data = ccma::algebra::DenseMatrixT<real>();
+	for(uint i = 0; i != seq_size; i++){
+		train_seq_data->get_row_data(i, seq_data);
+		auto store = ccma::algebra::DenseMatrixT<real>();
+		auto activation = ccma::algebra::DenseMatrixT<real>();
+
+		_weight->clone(store);
+		store->dot(seq_data);
+
+		if(i > 0){
+			auto pre_activation = ccma::algebra::DenseMatrixT<real>();
+			_pre_weight->clone(pre_activation);
+			pre_activation->dot(_activation[i-1]);
+			store->add(pre_activation);
+			delete pre_activation;
+		}
+
+		_store.push_back(store);
+
+		_act_weight->clone(activation);
+		activation->dot(store);
+		activation.tanh();
+		_activation.push_back(activation);
 	}
+	delete seq_data;
 }
+
+void Layer::back_propagation(ccma::algebra::BaseMatrixT<real>* train_seq_data,
+					         ccma::algebra::BaseMatrixT<real>* train_seq_label,
+						     bool debug = false){
+	feed_farward(train_seq_data, debug);
+
+	uint seq_size = train_seq_data->get_rows();
+
+}
+
 
 }//namespace rnn
 }//namespace algorithm
