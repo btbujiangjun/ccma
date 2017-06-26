b13b2473c01a09f0c1a2516ae84410d7eb35eb0a
btbujiangjun <jiangjun4@sina.com>
2017-06-26 18:38:37 +0800

    initialize

--- examples/algorithm/nn/TestRNN.cpp
+++ examples/algorithm/nn/TestRNN.cpp
@@ -7,9 +7,22 @@
 **********************************************/
 
 #include "utils/RNNHelper.h"
+#include "algorithm/rnn/RNN.h"
 
 int main(int argc, char** argv){
 	std::map<std::string, uint> map;
 	ccma::utils::RNNHelper helper(8000);
 	helper.read_word2index("data/word_to_index", map);
+
+	std::vector<ccma::algebra::BaseMatrixT<real>*> data_seq_data;
+	std::vector<ccma::algebra::BaseMatrixT<real>*> data_seq_label;
+
+	helper.read_seqdata("data/train_seq_data", &data_seq_data, "data/train_seq_label", &data_seq_label, 1000);
+
+    printf("Start training....\n");
+
+    auto rnn = new ccma::algorithm::rnn::RNN(8000, 100);
+    rnn->sgd(&data_seq_data, &data_seq_label);
+
+    delete rnn;
 }
