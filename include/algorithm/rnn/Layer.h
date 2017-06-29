990281de1dd2b5c36e43c1cde09b6ff873e7e068
btbujiangjun <jiangjun4@sina.com>
2017-06-29 18:51:03 +0800

    initialize

--- include/algorithm/rnn/Layer.h
+++ include/algorithm/rnn/Layer.h
@@ -27,62 +27,52 @@ public:
 	    _hidden_dim = hidden_dim;
         _bptt_truncate = bptt_truncate;
 
-		_weight = weight;
-		_pre_weight = pre_weight;
-		_act_weight = act_weight;
+//		_weight = weight;
+//		_pre_weight = pre_weight;
+//		_act_weight = act_weight;
 
-        _store = new ccma::algebra::DenseMatrixT<real>();
-        _activation = new ccma::algebra::DenseMatrixT<real>();
+//        _store = new ccma::algebra::DenseMatrixT<real>();
+//        _activation = new ccma::algebra::DenseMatrixT<real>();
 	
-		_derivate_weight = new ccma::algebra::DenseMatrixT<real>(_weight->get_rows(), _weight->get_cols());
-		_derivate_pre_weight = new ccma::algebra::DenseMatrixT<real>(_pre_weight->get_rows(), _pre_weight->get_cols());
-		_derivate_act_weight = new ccma::algebra::DenseMatrixT<real>(_act_weight->get_rows(), _act_weight->get_cols());
+//		_derivate_weight = new ccma::algebra::DenseMatrixT<real>(_weight->get_rows(), _weight->get_cols());
+//		_derivate_pre_weight = new ccma::algebra::DenseMatrixT<real>(_pre_weight->get_rows(), _pre_weight->get_cols());
+//		_derivate_act_weight = new ccma::algebra::DenseMatrixT<real>(_act_weight->get_rows(), _act_weight->get_cols());
 	}
 
-	~Layer(){
-		if(_store != nullptr){
-			delete _store;
-		}
-		if(_activation != nullptr){
-			delete _activation;
-		}
-	}
+	~Layer() = default;
 
-	void feed_farward(ccma::algebra::BaseMatrixT<real>* train_seq_data, bool debug = false);
+	void feed_farward(ccma::algebra::BaseMatrixT<real>* train_seq_data,
+                      ccma::algebra::BaseMatrixT<real>* weight,
+                      ccma::algebra::BaseMatrixT<real>* pre_weight,
+                      ccma::algebra::BaseMatrixT<real>* act_weight,
+                      ccma::algebra::BaseMatrixT<real>* store,
+                      ccma::algebra::BaseMatrixT<real>* activation,
+                      bool debug = false);
+
 	void back_propagation(ccma::algebra::BaseMatrixT<real>* train_seq_data,
 						  ccma::algebra::BaseMatrixT<real>* train_seq_label,
-						  bool debug = false);
-
-    ccma::algebra::BaseMatrixT<real>* get_activation(){
-        return _activation;
-    }
-
-    ccma::algebra::BaseMatrixT<real>* get_derivate_weight(){
-        return _derivate_weight;
-    }
-    ccma::algebra::BaseMatrixT<real>* get_derivate_pre_weight(){
-        return _derivate_pre_weight;
-    }
-    ccma::algebra::BaseMatrixT<real>* get_derivate_act_weight(){
-        return _derivate_act_weight;
-    }
-private:
-	void initialize(ccma::algebra::BaseMatrixT<real>* train_seq_data);
+                          ccma::algebra::BaseMatrixT<real>* weight,
+                          ccma::algebra::BaseMatrixT<real>* pre_weight,
+                          ccma::algebra::BaseMatrixT<real>* act_weight,
+                          ccma::algebra::BaseMatrixT<real>* derivate_weight,
+                          ccma::algebra::BaseMatrixT<real>* derivate_pre_weight,
+                          ccma::algebra::BaseMatrixT<real>* derivate_act_weight,
+                          bool debug = false);
 
 private:
 	uint _hidden_dim;
     uint _bptt_truncate;
 
-	ccma::algebra::BaseMatrixT<real>* _store;
-	ccma::algebra::BaseMatrixT<real>* _activation;
+//	ccma::algebra::BaseMatrixT<real>* _store;
+//	ccma::algebra::BaseMatrixT<real>* _activation;
 
-	ccma::algebra::BaseMatrixT<real>* _weight;
-	ccma::algebra::BaseMatrixT<real>* _pre_weight;
-	ccma::algebra::BaseMatrixT<real>* _act_weight;
+//	ccma::algebra::BaseMatrixT<real>* _weight;
+//	ccma::algebra::BaseMatrixT<real>* _pre_weight;
+//	ccma::algebra::BaseMatrixT<real>* _act_weight;
 	
-	ccma::algebra::BaseMatrixT<real>* _derivate_weight;
-	ccma::algebra::BaseMatrixT<real>* _derivate_pre_weight;
-	ccma::algebra::BaseMatrixT<real>* _derivate_act_weight;
+//	ccma::algebra::BaseMatrixT<real>* _derivate_weight;
+//	ccma::algebra::BaseMatrixT<real>* _derivate_pre_weight;
+//	ccma::algebra::BaseMatrixT<real>* _derivate_act_weight;
 	
 };//class Layer
 
